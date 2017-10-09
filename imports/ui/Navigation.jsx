5cb40493bf7863d23267dedc52fc907558a6580e
eacabello10 <ea.cabello10@uniandes.edu.co>
2017-10-08 21:52:25 -0500

    habemus boton huehuehue se podia hacer en 5 minutos pero me demore 2 horas

--- imports/ui/Navigation.jsx
+++ imports/ui/Navigation.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
+import "./Navigation.css";
+
 class Navigation extends Component {
     constructor(props){
         super(props);
@@ -10,25 +12,18 @@ class Navigation extends Component {
 
     render(){
         return (
-            <div  className="navigationBar">
-                <nav className="navbar navbar-inverse">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span> 
-                            </button>
-                            <a className="navbar-brand" href="#">WebSiteName</a>
-                        </div>
-                        <div className="collapse navbar-collapse" id="myNavbar">
-                            <ul className="nav navbar-nav">
-                                <li className="active"><AccountsUIWrapper />Hola</li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+        <div  className="navigationBar">
+            <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                    <div class="navbar-header">
+                    <a class="navbar-brand" href="#">HelpAStranger</a>
             </div>
+            <ul class="nav navbar-nav navbar-left">
+                <a className="boton-login"><AccountsUIWrapper></AccountsUIWrapper></a>
+            </ul>
+            </div>
+            </nav>
+        </div>
         )
     }
 }
